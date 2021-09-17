c2cf1acc0e5e50e2ef5b42e68fab62ee85b1f4d8
Leo MEYER <leo@webetdesign.com>
2021-09-17 15:19:22 +0200

    fix template missing

--- src/Controller/Admin/User/SecurityController.php
+++ src/Controller/Admin/User/SecurityController.php
@@ -26,7 +26,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('admin/security/login.html.twig', [
+        return $this->render('@WDUser/admin/security/login.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error
         ]);
