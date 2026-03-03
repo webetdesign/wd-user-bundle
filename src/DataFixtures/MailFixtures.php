8cad76f8b8bf2b0d0d06a2581bb2f5c74e021556
Jérôme VALDENAIRE <jerome@webetdesign.com>
2026-03-03 14:13:40 +0100

    Add composer.lock to track dependencies state
    This commit includes the generated `composer.lock` file to lock the project's dependencies to a specific state. Ensuring this file is tracked improves reproducibility and consistency across environments.

--- src/DataFixtures/MailFixtures.php
+++ src/DataFixtures/MailFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Persistence\ObjectManager;
 
 class MailFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $mails = [
             0 => [
