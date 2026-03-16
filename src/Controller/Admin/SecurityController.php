c77edb02422211cd1002606d1f72fdc138ca1ea1
Clement <clement@webetdesign.com>
2026-03-16 12:07:29 +0100

    Remove deprecated classes and update annotations
    Deprecated PasswordStrength classes were removed, and annotations were updated to use attributes across multiple files. This aligns with Symfony's recommended practices and ensures cleaner code maintenance.

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use WebEtDesign\UserBundle\Security\AdminFormLoginAuthenticator;
--- src/Controller/ExportZipController.php
+++ src/Controller/ExportZipController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class ExportZipController extends AbstractController
 {
